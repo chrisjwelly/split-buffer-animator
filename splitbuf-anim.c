926d296245aca824d5439c1699f27c94e4768e74
Christian James Welly <chrisjwelly@gmail.com>
2020-06-21 08:21:08 +0800

    Remove all references to gapbuf in splitbuf-anim

--- splitbuf-anim.c
+++ splitbuf-anim.c
@@ -75,148 +75,29 @@ image_write(const struct image *m, FILE *out)
     fflush(out);
 }
 
-struct gapbuf {
-    char *buf;
-    size_t total;
-    size_t front;
-    size_t gap;
-};
-
-void
-gapbuf_init(struct gapbuf *b, size_t init)
-{
-    b->total = b->gap = init;
-    b->front = 0;
-    b->buf = malloc(init);
-}
-
-void
-gapbuf_destroy(struct gapbuf *b)
-{
-    free(b->buf);
-    b->buf = 0;
-}
-
-void
-gapbuf_insert(struct gapbuf *b, int c)
-{
-    if (!b->gap) {
-        size_t back = b->total - b->front;
-        b->gap = b->total;
-        b->total *= 2;
-        b->buf = realloc(b->buf, b->total);
-        memmove(b->buf + b->front + b->gap, b->buf + b->front, back);
-    }
-    b->buf[b->front] = c;
-    b->front++;
-    b->gap--;
-}
-
-void
-gapbuf_inserts(struct gapbuf *b, const char *s)
-{
-    size_t len = strlen(s);
-    while (b->gap < len) {
-        b->gap = 0;
-        gapbuf_insert(b, 0);
-        b->front--;
-    }
-    memcpy(b->buf + b->front, s, len);
-    b->front += len;
-    b->gap -= len;
-}
-
-void
-gapbuf_move(struct gapbuf *b, ptrdiff_t amt)
-{
-    size_t len;
-    char *dst, *src;
-    if (amt < 0) {
-        len = -amt;
-        if (len > b->front)
-            len = b->front;
-        dst = b->buf + b->front + b->gap - len;
-        src = b->buf + b->front - len;
-        b->front -= len;
-    } else {
-        size_t back = b->total - b->front - b->gap;
-        len = amt;
-        if (len > back)
-            len = back;
-        dst = b->buf + b->front;
-        src = b->buf + b->front + b->gap;
-        b->front += len;
-    }
-    memmove(dst, src, len);
-}
-
-void
-gapbuf_backward(struct gapbuf *b)
-{
-    if (b->front > 0) {
-        b->buf[b->front + b->gap - 1] = b->buf[b->front - 1];
-        b->front--;
-    }
-}
-
-void
-gapbuf_forward(struct gapbuf *b)
-{
-    size_t back = b->total - b->front + b->gap;
-    if (back > 0) {
-        b->buf[b->front] = b->buf[b->front + b->gap];
-        b->front++;
-    }
-}
-
-void
-gapbuf_delete(struct gapbuf *b)
-{
-    if (b->total > b->front + b->gap)
-        b->gap++;
-}
-
-void
-gapbuf_backspace(struct gapbuf *b)
-{
-    if (b->front) {
-        b->front--;
-        b->gap++;
-    }
-}
-
-void
-gapbuf_write(const struct gapbuf *b, FILE *out)
-{
-    fwrite(b->buf, 1, b->front, out);
-    char *back_start = b->buf + b->front + b->gap;
-    size_t back_len = b->total - b->front - b->gap;
-    fwrite(back_start, 1, back_len, out);
-}
-
-#define GAPBUF_SCALE      16
-#define GAPBUF_FONTSCALE  32
-#define GAPBUF_BG         0xffffffUL
-#define GAPBUF_FG         0x7f7f7fUL
+#define SPLITBUF_SCALE      16
+#define SPLITBUF_FONTSCALE  32
+#define SPLITBUF_BG         0xffffffUL
+#define SPLITBUF_FG         0x7f7f7fUL
 
 static void
 draw_block_preGap(struct image *m, int i)
 {
-    int x0 = i * GAPBUF_SCALE + GAPBUF_SCALE * 1 / 8;
-    int y0 = GAPBUF_FONTSCALE + GAPBUF_SCALE * 1 / 8;
-    int x1 = i * GAPBUF_SCALE + GAPBUF_SCALE * 7 / 8;
-    int y1 = GAPBUF_FONTSCALE + GAPBUF_SCALE * 7 / 8;
-    image_rect(m, x0, y0, x1, y1, GAPBUF_FG);
+    int x0 = i * SPLITBUF_SCALE + SPLITBUF_SCALE * 1 / 8;
+    int y0 = SPLITBUF_FONTSCALE + SPLITBUF_SCALE * 1 / 8;
+    int x1 = i * SPLITBUF_SCALE + SPLITBUF_SCALE * 7 / 8;
+    int y1 = SPLITBUF_FONTSCALE + SPLITBUF_SCALE * 7 / 8;
+    image_rect(m, x0, y0, x1, y1, SPLITBUF_FG);
 }
 
 static void
 draw_block_postGap(struct image *m, int i)
 {
-    int x0 = i * GAPBUF_SCALE + GAPBUF_SCALE * 1 / 8;
-    int y0 = GAPBUF_FONTSCALE + GAPBUF_SCALE * 1 / 8 + GAPBUF_SCALE;
-    int x1 = i * GAPBUF_SCALE + GAPBUF_SCALE * 7 / 8;
-    int y1 = GAPBUF_FONTSCALE + GAPBUF_SCALE * 7 / 8 + GAPBUF_SCALE;
-    image_rect(m, x0, y0, x1, y1, GAPBUF_FG);
+    int x0 = i * SPLITBUF_SCALE + SPLITBUF_SCALE * 1 / 8;
+    int y0 = SPLITBUF_FONTSCALE + SPLITBUF_SCALE * 1 / 8 + SPLITBUF_SCALE;
+    int x1 = i * SPLITBUF_SCALE + SPLITBUF_SCALE * 7 / 8;
+    int y1 = SPLITBUF_FONTSCALE + SPLITBUF_SCALE * 7 / 8 + SPLITBUF_SCALE;
+    image_rect(m, x0, y0, x1, y1, SPLITBUF_FG);
 }
 
 static void
@@ -228,7 +109,7 @@ draw_char(struct image *m, int i, int c, const struct image *font, int invert)
     int fy = c / 16 - 2;
     int fw = font->w / 16;
     int fh = font->h / 6;
-    int h = GAPBUF_FONTSCALE;
+    int h = SPLITBUF_FONTSCALE;
     int w = fw * h / fh;
     int bx = w * i;
     for (int y = 0; y < h; y++) {
@@ -250,7 +131,7 @@ draw_char_preGap(struct image *m, int i, int c, const struct image *font, int in
     int fy = c / 16 - 2;
     int fw = font->w / 16;
     int fh = font->h / 6;
-    int h = GAPBUF_FONTSCALE / 2;
+    int h = SPLITBUF_FONTSCALE / 2;
     int w = fw * h / fh;
     int bx = w * i;
     for (int y = 0; y < h; y++) {
@@ -258,7 +139,7 @@ draw_char_preGap(struct image *m, int i, int c, const struct image *font, int in
             float sx = fx * fw + (float)x * fw / w;
             float sy = fy * fh + (float)y * fh / h;
             unsigned long rgb = image_get(font, sx, sy);
-            image_set(m, bx + x, y + GAPBUF_FONTSCALE, invert ? -1UL ^ rgb : rgb);
+            image_set(m, bx + x, y + SPLITBUF_FONTSCALE, invert ? -1UL ^ rgb : rgb);
         }
     }
 }
@@ -272,7 +153,7 @@ draw_char_postGap(struct image *m, int i, int c, const struct image *font, int i
     int fy = c / 16 - 2;
     int fw = font->w / 16;
     int fh = font->h / 6;
-    int h = GAPBUF_FONTSCALE / 2;
+    int h = SPLITBUF_FONTSCALE / 2;
     int w = fw * h / fh;
     int bx = w * i;
     for (int y = 0; y < h; y++) {
@@ -280,40 +161,18 @@ draw_char_postGap(struct image *m, int i, int c, const struct image *font, int i
             float sx = fx * fw + (float)x * fw / w;
             float sy = fy * fh + (float)y * fh / h;
             unsigned long rgb = image_get(font, sx, sy);
-            image_set(m, bx + x, y + GAPBUF_FONTSCALE * 3/2, invert ? -1UL ^ rgb : rgb);
+            image_set(m, bx + x, y + SPLITBUF_FONTSCALE * 3/2, invert ? -1UL ^ rgb : rgb);
         }
     }
-}
-
-struct image *
-gapbuf_draw(const struct gapbuf *b, const struct image *font)
-{
-    int w = b->total * GAPBUF_SCALE;
-    int h = GAPBUF_FONTSCALE + GAPBUF_SCALE;
-    struct image *m = image_create(w, h);
-    image_rect(m, 0, 0, w, h, GAPBUF_BG);
-
-    for (size_t i = 0; i < b->front; i++) {
-        // draw_block(m, i);
-        draw_char(m, i, b->buf[i], font, 0);
-    }
-    for (size_t i = b->front + b->gap; i < b->total; i++) {
-        // draw_block(m, i);
-        draw_char(m, i - b->gap, b->buf[i], font, i == b->front + b->gap);
-    }
-    /* Always draw the cursor */
-    if (b->total == b->front + b->gap)
-        draw_char(m, b->front, 0, font, 1);
-    return m;
 }
 
 struct image *
 splitbuf_draw(SplitBuffer *b, const struct image *font)
 {
     int w = 608;
-    int h = GAPBUF_FONTSCALE + GAPBUF_SCALE + GAPBUF_SCALE;
+    int h = SPLITBUF_FONTSCALE + SPLITBUF_SCALE + SPLITBUF_SCALE;
     struct image *m = image_create(w, h);
-    image_rect(m, 0, 0, w, h, GAPBUF_BG);
+    image_rect(m, 0, 0, w, h, SPLITBUF_BG);
 
     for (size_t i = 0; i < b->preGap->size; i++) {
       draw_char_preGap(m, i, b->preGap->items[i], font, 0);
